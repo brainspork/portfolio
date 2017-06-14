7dc69f6a3c087e4da5a3d300eee21c682b4ff42f
Anthony Konschuh <silv.amee@gmail.com>
2017-06-14 11:59:37 -0700

    adds imgs, creates obj constr, instantiates raw data, adds jquery, inserts in dom

---
+++ scripts/projectData.js
@@ -0,0 +1,25 @@
+'use strict';
+
+var rawData = [
+  {
+    img: 'img/dunGen.png',
+    name: 'DunGen',
+    date: '05 - 21 - 2017',
+    url: 'https://loaye.github.io/DunGen-Dragons/home/index.html',
+    info: 'Generates random characters based on a number of choices a user can make .....'
+  },
+  {
+    img: 'img/busmall.png',
+    name: 'Bus Mall',
+    date: '05 - 14 - 2017',
+    url: 'https://brainspork.github.io/busmall/',
+    info: 'random picture generator for a ......'
+  },
+  {
+    img: 'img/salmon.png',
+    name: 'Salmon Cookies',
+    date: '05 - 07 - 2017',
+    url: 'https://brainspork.github.io/salmon_cookies/',
+    info: 'website for seattle startup company....'
+  }
+];
