9bf18451b2b6d48c18f952a22cb694dd8242ff47
Anthony Konschuh <silv.amee@gmail.com>
2017-06-19 11:15:06 -0700

    adds second animation
    Adds second animation to animation array and animation.css

--- scripts/projectData.js
+++ scripts/projectData.js
@@ -26,5 +26,5 @@ var rawData = [
 
 var animations = [
   '<div data-index="0" id="backone" class="box-animation animation"><div id="ballone" class="box-animation"></div></div><div id="backtwo" class="box-animation"><div id="balltwo" class="box-animation"></div></div>',
-  '<p data-index="1">works</p>', '<p data-index="2" >Works 2s</p>'
+  '<div data-index="1" id="outer"><div id="circleone"></div><div id="circletwo"></div><div id="circlethree"></div><div id="cirlcefour"></div></div>'
 ];
